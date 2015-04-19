aed5fce93eb0da4a491fd6ffc82ec622f2bc51b2
Jordeen Chang <jordeenchang@gmail.com>
2015-04-19 16:57:38 -0700

    navigation links work

--- yomu/yomu/urls.py
+++ yomu/yomu/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from yomu.views import home
 
 urlpatterns = [
-    url(r'^$', 'yomu.views.home', name='home'),
-
+    url(r'^$', 'yomu.views.home', name='home_url_name'),
+    url(r'^mybooks/$', 'yomu.views.mybooks', name='mybooks_url_name'),
+    url(r'^profile/$', 'yomu.views.profile', name='profile_url_name'),
 ]
