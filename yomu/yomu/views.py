508451c5db55a97281c399151e2c49f9bd713897
Jordeen Chang <jordeenchang@gmail.com>
2015-04-19 17:42:04 -0700

    removing some headers

--- yomu/yomu/views.py
+++ yomu/yomu/views.py
@@ -4,7 +4,6 @@ from django.shortcuts import render
 
 def home(request):
     content = {
-    'title' : 'My First Post',
     'author' : 'Giles',
     'date' : '18th September 2011',
     'body' : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam cursus tempus dui, ut vulputate nisl eleifend eget. Aenean justo felis, dapibus quis vulputate at, porta et dolor. Praesent enim libero, malesuada nec vestibulum vitae, fermentum nec ligula. Etiam eget convallis turpis. Donec non sem justo.',
@@ -13,18 +12,16 @@ def home(request):
 
 def mybooks(request):
     content = {
-    'title' : 'My First Post',
     'author' : 'Giles',
     'date' : '18th September 2011',
-    'body' : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam cursus tempus dui, ut vulputate nisl eleifend eget. Aenean justo felis, dapibus quis vulputate at, porta et dolor. Praesent enim libero, malesuada nec vestibulum vitae, fermentum nec ligula. Etiam eget convallis turpis. Donec non sem justo.',
+    'body' : 'Blah blah blah dolor sit amet, consectetur blah blah elit. Etiam cursus tempus dui, ut vulputate nisl blah blah eget. Aenean justo felis, dapibus quis blah blah at, porta et dolor. Praesent enim libero, malesuada nec vestibulum vitae, fermentum nec ligula. Etiam eget convallis turpis. Donec non sem justo.',
     }
     return render(request, 'mybooks.html', content)
 
 def profile(request):
     content = {
-    'title' : 'My Profile Post',
     'author' : 'Giles',
     'date' : '18th September 2011',
-    'body' : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam cursus tempus dui, ut vulputate nisl eleifend eget. Aenean justo felis, dapibus quis vulputate at, porta et dolor. Praesent enim libero, malesuada nec vestibulum vitae, fermentum nec ligula. Etiam eget convallis turpis. Donec non sem justo.',
+    'body' : 'Lorem ipsum dolor sit amet, consectetur adipisci dis da bodie hello.',
     }
     return render(request, 'profile.html', content)
